2b58fec25f4755a4e216080db351dff4468acaf8
Martin Fournier <martinfou@gmail.com>
2021-03-08 14:54:32 -0500

    hardcode chart values

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,45 @@
 <template>
   <div class="home">
     <HelloWorld msg="Welcome to Your Vue.js App" />
+    <v-row>
+      <v-col cols="6">
+        <BarChart :chartdata="chartdata" :options="options" />
+      </v-col>
+    </v-row>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
+import BarChart from '@/components/BarChart.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
-  }
+    HelloWorld,
+    BarChart
+  },
+  data: () => ({
+    chartdata: {
+      labels: ['January', 'February'],
+      datasets: [
+        {
+          label: 'Data One',
+          backgroundColor: '#f87979',
+          data: [40, 20]
+        },
+        {
+          labeld: 'Data Two',
+          backgroundColor: 'blue',
+          data: [10, 30]
+        }
+      ]
+    },
+    options: {
+      responsive: true,
+      maintainAspectRatio: false
+    }
+  })
 }
 </script>
